7b7b2ebf2faef136917aaf7b076972cd559ee25b
abnermolina133089 <abnermolina133089@gmail.com>
2022-04-09 12:24:58 -0600

    1er commit

---
+++ FinalCursoGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FinalCursoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Examen final Git-------------");
+            Console.WriteLine("----------------Menu-----------------");
+            Console.WriteLine("------Seleccione opcion--------------");
+            Console.WriteLine("------1 ingresar nuevo---------------");
+            Console.WriteLine("------2 editar registro--------------");
+            Console.WriteLine("------3 eliminar registro------------");
+            Console.WriteLine("------3 buscar registro--------------");
+            Console.WriteLine("-------------------------------------");
+
+        }
+    }
+}
